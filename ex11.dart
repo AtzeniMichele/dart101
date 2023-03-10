742de0528ca46fbf5196eb8bd19880b66e289ebd
AtzeniMichele <michele.atzeni@studenti.unipd.it>
2023-03-10 11:49:28 +0100

    comments added

--- ex11.dart
+++ ex11.dart
@@ -1,6 +1,9 @@
+// Write a program (function) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
 import 'dart:math';
 
 void main() {
+  // initialize random and use it to generate ten random numbers between [0, 10[
   final random = Random();
   List<int> randList = List.generate(10, (_) => random.nextInt(10));
 
@@ -11,9 +14,12 @@ void main() {
 List<int> removeDuplicates(List<int> initialList) {
   final List<int> secondList = [];
   for (int element in initialList) {
+    // check wether an element is already inside the new list
     if (secondList.contains(element)) {
+      // yes, the we do not want to add it into the new list
       continue;
     } else {
+      // no, then add it
       secondList.add(element);
     }
   }
--- ex12.dart
+++ ex12.dart
@@ -1,23 +1,31 @@
+// Write a password generator in Dart. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters,
+//uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time.
+// Include your run-time code in a main method
+
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
 void main() {
+  // we want to generate a weak passowrd
   String type = 'weak';
   print('I am generating a $type password');
   String pw = passwordGenerator(type);
   print(pw);
 
+  // we want to generate a medium passowrd
   type = 'medium';
   print('I am generating a $type password');
   String pw2 = passwordGenerator(type);
   print(pw2);
 
+// we want to generate a strong passowrd
   type = 'strong';
   print('I am generating a $type password');
   String pw3 = passwordGenerator(type);
   print(pw3);
 
+  // what if we give as input a wrong word?
   type = 'easy';
   print('I am generating a $type password');
   String pw4 = passwordGenerator(type);
@@ -26,9 +34,12 @@ void main() {
 
 // generator of pw
 String shuffleGenerator(int strength) {
+  // cryptographically secure random number generator
   final random = Random.secure();
   List<int> intList = List.generate(strength, (_) => random.nextInt(255));
+  // convert bytes into ASCII characters
   List<String> charList = base64UrlEncode(intList).split('').toList();
+  // be even more random
   charList.shuffle();
   return charList.join('');
 }
@@ -36,7 +47,7 @@ String shuffleGenerator(int strength) {
 // controller of what type of pw has been requested
 String passwordGenerator(String strength) {
   final String pw;
-  switch (strength) {
+  switch (strength.toLowerCase()) {
     case 'weak':
       pw = shuffleGenerator(5);
       break;
